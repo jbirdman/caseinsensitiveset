d593ad309f1c6b7f416c92b1aa1d5061e2e3d3a5
Jeffrey Bird <jeffrey.bird@jcu.edu.au>
2022-11-30 09:00:27 +1000

    Added implementation.

---
+++ caseinsensitiveset.go
@@ -0,0 +1,64 @@
+package ldapx
+
+import (
+	mapset "github.com/deckarep/golang-set/v2"
+	"strings"
+)
+
+type CaseInsensitiveSet struct {
+	s          mapset.Set[string]
+	elementMap map[string]string
+}
+
+func NewCaseInsensitiveSet(values ...string) *CaseInsensitiveSet {
+	set := CaseInsensitiveSet{
+		s:          mapset.NewSet[string](),
+		elementMap: make(map[string]string),
+	}
+
+	for _, v := range values {
+		set.elementMap[strings.ToLower(v)] = v
+		set.s.Add(strings.ToLower(v))
+	}
+
+	return &set
+}
+
+func (s *CaseInsensitiveSet) Add(value string) bool {
+	s.elementMap[strings.ToLower(value)] = value
+	return s.s.Add(strings.ToLower(value))
+}
+
+func (s *CaseInsensitiveSet) Remove(value string) {
+	delete(s.elementMap, strings.ToLower(value))
+	s.s.Remove(strings.ToLower(value))
+}
+
+func (s *CaseInsensitiveSet) Difference(s1 *CaseInsensitiveSet) *CaseInsensitiveSet {
+	diffSet := NewCaseInsensitiveSet()
+	for _, v := range s.s.Difference(s1.s).ToSlice() {
+		diffSet.Add(s.elementMap[v])
+	}
+	return diffSet
+}
+
+func (s *CaseInsensitiveSet) ToSlice() []string {
+	vals := make([]string, 0, len(s.elementMap))
+
+	for _, v := range s.elementMap {
+		vals = append(vals, v)
+	}
+	return vals
+}
+
+func (s *CaseInsensitiveSet) Contains(value ...string) bool {
+	return s.s.Contains(mapStringSlice(value, strings.ToLower)...)
+}
+
+func mapStringSlice(t []string, f func(string) string) []string {
+	s := make([]string, len(t))
+	for i, v := range t {
+		s[i] = f(v)
+	}
+	return s
+}
--- go.mod
+++ go.mod
@@ -2,3 +2,4 @@ module github.com/jbirdman/caseinsensitiveset
 
 go 1.18
 
+require github.com/deckarep/golang-set/v2 v2.1.0
